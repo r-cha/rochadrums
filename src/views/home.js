a1aa1145ecd914255731482f228ad39d996963ba
Robert Chandler <robert@akchandler.com>
2022-07-29 00:29:53 -0700

    Clean up the generated code (#2)

--- src/views/home.js
+++ src/views/home.js
@@ -4,12 +4,44 @@ import { Helmet } from 'react-helmet'
 
 import './home.css'
 
+import favicon from '../assets/favicon.ico'
+import hero_landscape from "../assets/hero_landscape.jpg"
+import hero_portrait from "../assets/hero_portrait.jpg"
+
+function PortfolioCard(props) {
+  return (
+    <a
+      href={props.link}
+      target="_blank"
+      rel="noreferrer noopener"
+      className="home-portfolio-card-link"
+    >
+      <div className="home-portfolio-card">
+        <span className="home-portfolio-title card-Heading">{props.title}</span>
+        <span className="home-portfolio-artist card-Text">{props.artist}</span>
+      </div>
+    </a>
+  )
+}
+
+const ContactButton = (props) => {
+  return (
+    <a
+    href="mailto:robert@rochadrums.com?subject=rochadrums request"
+    className="home-cta-btn anchor button"
+  >
+    Contact
+  </a>
+  )
+}
+
 const Home = (props) => {
   return (
     <div className="home-container">
       <Helmet>
         <title>rochadrums</title>
         <meta property="og:title" content="rochadrums" />
+        <link rel="icon" type="image/png" href={favicon} sizes="32x32" />
       </Helmet>
       <div data-role="Header" className="home-navbar-container">
         <div className="home-navbar">
@@ -20,25 +52,27 @@ const Home = (props) => {
           </div>
           <div className="home-right-side">
             <div className="home-links-container">
-              <span className="home-link anchor">Home</span>
-              <a href="#process" className="home-link1 anchor">
+              <a href="#home" className="home-link anchor">
+                Home
+              </a>
+              <a href="#services" className="home-link anchor">
                 Services
               </a>
-              <a href="#portfolio" className="home-link2 anchor">
+              <a href="#portfolio" className="home-link anchor">
                 Music
               </a>
-              <a href="#contact" className="home-link3 anchor">
+              <a href="#contact" className="home-link-endcap anchor">
                 Contact
               </a>
             </div>
           </div>
         </div>
       </div>
-      <div id="resources" className="home-hero">
+      <div id="home" className="home-hero" style={{backgroundImage: `url(${hero_landscape})`}}>
         <div className="home-content-container">
           <div className="home-hero-text">
             <h1 className="home-heading section-Heading">
-              <span className="home-text01">Ro</span>
+              <span className="home-head-emphasis">Ro</span>
               <span>
                 bert
                 <span
@@ -47,10 +81,10 @@ const Home = (props) => {
                   }}
                 />
               </span>
-              <span className="home-text03">Cha</span>
+              <span className="home-head-emphasis">Cha</span>
               <span>ndler</span>
             </h1>
-            <span className="home-text05 section-Text">
+            <span className="home-hero-intro-text section-Text">
               <span>
                 After 15 years of drumming experience, Robert is ready to share
                 his craft with others publicly.
@@ -64,48 +98,37 @@ const Home = (props) => {
                 session musician.
               </span>
             </span>
-            <a
-              href="mailto:robert@rochadrums.com?subject=rochadrums request"
-              className="home-cta-btn anchor button"
-            >
-              Contact
-            </a>
+            <ContactButton />
           </div>
         </div>
       </div>
-      <div id="process" className="home-services">
+      <div id="services" className="home-services">
         <div className="home-heading-container">
-          <h1 className="home-text09 section-Heading">Services</h1>
+          <h1 className="home-section-heading section-Heading">Services</h1>
         </div>
         <div className="home-cards-container">
           <div className="home-service-card">
-            <h3 className="home-text10 card-Heading">Lessons</h3>
-            <span className="home-text11 card-Text">
+            <h3 className="home-service-card-heading card-Heading">Lessons</h3>
+            <span className="home-service-card-text card-Text">
               <span>
                 Lessons are currently available
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
               </span>
-              <span className="home-text13">in-person</span>
+              <span className="home-services-emphasis">
+                {' '}
+                in-person
+              </span>
               <span>
                 {' '}
                 in Phoenix, AZ.
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
               </span>
               <span>
-                Robert has taught all ages — from 3 to 73 — and skill levels.‍
+                {' '}
+                Robert has taught all ages — from 3 to 73 — and skill levels.
               </span>
               <br></br>
               <span></span>
               <br></br>
-              <span className="home-text19">Beginner:</span>
+              <span className="home-services-emphasis">Beginner:</span>
               <span>
                 {' '}
                 Looking to start a new hobby, learn another instrument, or be
@@ -113,7 +136,7 @@ const Home = (props) => {
                 with an unassailable foundation in the percussive arts.
               </span>
               <br></br>
-              <span className="home-text22">Intermediate:</span>
+              <span className="home-services-emphasis">Intermediate:</span>
               <span>
                 {' '}
                 Ready to step up your skills, prepare for an audition, or
@@ -121,7 +144,7 @@ const Home = (props) => {
                 your drumming to the next level.
               </span>
               <br></br>
-              <span className="home-text25">Advanced:</span>
+              <span className="home-services-emphasis">Advanced:</span>
               <span>
                 {' '}
                 Robert is looking for a drumming partner at
@@ -133,22 +156,18 @@ const Home = (props) => {
               <br></br>
               <span></span>
               <br></br>
-              <span className="home-text30">
+              <span className="home-services-emphasis">
                 Rates:
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
               </span>
               <span>
+                {' '}
                 $35/hour a la carte, or $70/month for weekly 1-hour lessons
               </span>
             </span>
           </div>
-          <div className="home-service-card1">
-            <h3 className="home-text32 card-Heading">Live</h3>
-            <span className="home-text33 card-Text">
+          <div className="home-service-card">
+            <h3 className="home-service-card-heading card-Heading">Live</h3>
+            <span className="home-service-card-text card-Text">
               <span>
                 Robert has over 10 years of experience in live performance,
                 including contemporary Christian worship, jazz, and concert
@@ -159,7 +178,7 @@ const Home = (props) => {
               <br></br>
               <span></span>
               <br></br>
-              <span className="home-text38">Churches:</span>
+              <span className="home-services-emphasis">Churches:</span>
               <span>
                 {' '}
                 Sunday availability is limited, but feel free to reach out for
@@ -168,22 +187,18 @@ const Home = (props) => {
               <br></br>
               <span></span>
               <br></br>
-              <span className="home-text30">
+              <span className="home-services-emphasis">
                 Rates:
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
               </span>
               <span>
+                {' '}
                 Variable, but a testimonial is always appreciated!
               </span>
             </span>
           </div>
-          <div className="home-service-card2">
-            <h3 className="home-text44 card-Heading">Studio</h3>
-            <span className="home-text45 card-Text">
+          <div className="home-service-card">
+            <h3 className="home-service-card-heading card-Heading">Studio</h3>
+            <span className="home-service-card-text card-Text">
               <span>
                 Studio recording is a secret passion for Robert. He has
                 collaborated with artists in every step of the process, from
@@ -193,7 +208,7 @@ const Home = (props) => {
               <br></br>
               <span></span>
               <br></br>
-              <span className="home-text50">Demos:</span>
+              <span className="home-services-emphasis">Demos:</span>
               <span>
                 {' '}
                 Need a quick scratch track on drums? Robert has a limited stereo
@@ -201,25 +216,21 @@ const Home = (props) => {
                 a track for you within 48 hours.
               </span>
               <br></br>
-              <span className="home-text53">Full session:</span>
+              <span className="home-services-emphasis">Full session:</span>
               <span>
                 {' '}
-                Robert’s recording hardware is limited, but if you are recording
-                in the Phoenix metro area, he’s ready to throw in on studio time
+                Robert&apos;s recording hardware is limited, but if you are recording
+                in the Phoenix metro area, he&apos;s ready to throw in on studio time
                 for your album or EP.
               </span>
               <br></br>
               <span></span>
               <br></br>
-              <span className="home-text30">
+              <span className="home-services-emphasis">
                 Rates:
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: ' ',
-                  }}
-                />
               </span>
               <span>
+                {' '}
                 A testimonial and a master of the final result.
               </span>
             </span>
@@ -228,97 +239,72 @@ const Home = (props) => {
       </div>
       <div className="home-section-separator"></div>
       <div id="portfolio" className="home-portfolio">
-        <div className="home-heading-container1">
-          <h1 className="home-text59 section-Heading">Music</h1>
+        <div className="home-heading-container">
+          <h1 className="home-section-heading section-Heading">Music</h1>
         </div>
-        <div className="home-cards-container1">
-          <a
-            href="https://open.spotify.com/album/2MybIcQlbTG7wowDJLTYDN?si=97yO3qBGRjiNd2m8Ktthlg"
-            target="_blank"
-            rel="noreferrer noopener"
-            className="home-link4"
-          >
-            <div className="home-team-card">
-              <span className="home-name card-Heading">
-                It&apos;s Always Been You
-              </span>
-              <span className="home-position card-Text">Antioch Phoenix</span>
-            </div>
-          </a>
-          <a
-            href="https://open.spotify.com/track/1AyFbnFkMbtaJwPmmu5m1k?si=991a090b61c64ca7"
-            target="_blank"
-            rel="noreferrer noopener"
-            className="home-link5"
-          >
-            <div className="home-team-card1">
-              <span className="home-name1 card-Heading">One Day Closer to You</span>
-              <span className="home-position1 card-Text">Anna Philippe</span>
-            </div>
-          </a>
-          <a
-            href="https://open.spotify.com/track/6es8La47ZnWxLTLvXyoNox?si=33eee988f9fa4b39"
-            target="_blank"
-            rel="noreferrer noopener"
-            className="home-link6"
-          >
-            <div className="home-team-card2">
-              <span className="home-name2 card-Heading">River of Life</span>
-              <span className="home-position2 card-Text">Emma Terlizzi</span>
-            </div>
-          </a>
+        <div className="home-portfolio-container">
+          <PortfolioCard
+            link="https://open.spotify.com/album/2MybIcQlbTG7wowDJLTYDN?si=97yO3qBGRjiNd2m8Ktthlg"
+            title="It's Always Been You"
+            artist="Antioch Phoenix">
+          </ PortfolioCard>
+          <PortfolioCard
+            link="https://open.spotify.com/track/1AyFbnFkMbtaJwPmmu5m1k?si=991a090b61c64ca7"
+            title="One Day Closer to You"
+            artist="Anna Philippe">
+          </ PortfolioCard>
+          <PortfolioCard
+            link="https://open.spotify.com/track/6es8La47ZnWxLTLvXyoNox?si=33eee988f9fa4b39"
+            title="River of Life"
+            artist="Emma Terlizzi">
+          </ PortfolioCard>
         </div>
       </div>
-      <div className="home-section-separator1"></div>
-      <div id="contact" className="home-get-in-touch">
-        <div className="home-content-container1">
-          <div className="home-hero-text1">
-            <h1 className="home-heading1 section-Heading">
+      <div className="home-section-separator"></div>
+      <div id="contact" className="home-get-in-touch" style={{backgroundImage: `url(${hero_portrait})`}}>
+        <div className="home-content-container">
+          <div className="home-hero-text">
+            <h1 className="home-heading section-Heading">
               <span>Get In Touch</span>
             </h1>
-            <div className="home-content-container2">
+            <div className="home-contact-container">
               <svg viewBox="0 0 1024 1024" className="home-icon">
                 <path d="M854 342v-86l-342 214-342-214v86l342 212zM854 170q34 0 59 26t25 60v512q0 34-25 60t-59 26h-684q-34 0-59-26t-25-60v-512q0-34 25-60t59-26h684z"></path>
               </svg>
               <a
                 href="mailto:robert@rochadrums.com?subject=rochadrums request"
-                className="home-text61 section-Text"
+                className="home-contact-method-text section-Text"
               >
                 <span>robert@rochadrums.com</span>
               </a>
-              <svg viewBox="0 0 877.7142857142857 1024" className="home-icon2">
+              <svg viewBox="0 0 877.7142857142857 1024" className="home-icon">
                 <path d="M585.143 512c0-80.571-65.714-146.286-146.286-146.286s-146.286 65.714-146.286 146.286 65.714 146.286 146.286 146.286 146.286-65.714 146.286-146.286zM664 512c0 124.571-100.571 225.143-225.143 225.143s-225.143-100.571-225.143-225.143 100.571-225.143 225.143-225.143 225.143 100.571 225.143 225.143zM725.714 277.714c0 29.143-23.429 52.571-52.571 52.571s-52.571-23.429-52.571-52.571 23.429-52.571 52.571-52.571 52.571 23.429 52.571 52.571zM438.857 152c-64 0-201.143-5.143-258.857 17.714-20 8-34.857 17.714-50.286 33.143s-25.143 30.286-33.143 50.286c-22.857 57.714-17.714 194.857-17.714 258.857s-5.143 201.143 17.714 258.857c8 20 17.714 34.857 33.143 50.286s30.286 25.143 50.286 33.143c57.714 22.857 194.857 17.714 258.857 17.714s201.143 5.143 258.857-17.714c20-8 34.857-17.714 50.286-33.143s25.143-30.286 33.143-50.286c22.857-57.714 17.714-194.857 17.714-258.857s5.143-201.143-17.714-258.857c-8-20-17.714-34.857-33.143-50.286s-30.286-25.143-50.286-33.143c-57.714-22.857-194.857-17.714-258.857-17.714zM877.714 512c0 60.571 0.571 120.571-2.857 181.143-3.429 70.286-19.429 132.571-70.857 184s-113.714 67.429-184 70.857c-60.571 3.429-120.571 2.857-181.143 2.857s-120.571 0.571-181.143-2.857c-70.286-3.429-132.571-19.429-184-70.857s-67.429-113.714-70.857-184c-3.429-60.571-2.857-120.571-2.857-181.143s-0.571-120.571 2.857-181.143c3.429-70.286 19.429-132.571 70.857-184s113.714-67.429 184-70.857c60.571-3.429 120.571-2.857 181.143-2.857s120.571-0.571 181.143 2.857c70.286 3.429 132.571 19.429 184 70.857s67.429 113.714 70.857 184c3.429 60.571 2.857 120.571 2.857 181.143z"></path>
               </svg>
               <a
                 href="https://www.instagram.com/rochadrums/"
                 target="_blank"
                 rel="noreferrer noopener"
-                className="home-text63 section-Text"
+                className="home-contact-method-text section-Text"
               >
                 <span>rochadrums</span>
               </a>
-              <svg viewBox="0 0 1024 1024" className="home-icon4">
+              <svg viewBox="0 0 1024 1024" className="home-icon">
                 <path d="M938.667 426.667c0-117.803-47.787-224.555-124.971-301.696s-183.893-124.971-301.696-124.971-224.555 47.787-301.696 124.971-124.971 183.893-124.971 301.696c0 24.277 2.261 48.128 6.4 71.509 11.691 66.048 38.357 128.171 71.765 184.32 116.565 195.883 324.821 334.336 324.821 334.336 14.123 9.259 32.64 9.771 47.317 0 0 0 208.299-138.453 324.821-334.336 33.408-56.149 60.075-118.272 71.765-184.32 4.181-23.381 6.443-47.232 6.443-71.509zM853.333 426.667c0 18.944-1.749 37.845-5.077 56.661-9.429 53.333-31.445 105.728-61.099 155.563-83.285 139.947-220.416 249.643-275.157 290.091-54.784-40.448-191.872-150.144-275.157-290.091-29.653-49.835-51.669-102.229-61.099-155.563-3.328-18.816-5.077-37.717-5.077-56.661 0-94.251 38.144-179.541 99.968-241.365s147.115-99.968 241.365-99.968 179.541 38.144 241.365 99.968 99.968 147.115 99.968 241.365zM682.667 426.667c0-47.104-19.157-89.856-50.005-120.661s-73.557-50.005-120.661-50.005-89.856 19.157-120.661 50.005-50.005 73.557-50.005 120.661 19.157 89.856 50.005 120.661 73.557 50.005 120.661 50.005 89.856-19.157 120.661-50.005 50.005-73.557 50.005-120.661zM597.333 426.667c0 23.595-9.515 44.843-25.003 60.331s-36.736 25.003-60.331 25.003-44.843-9.515-60.331-25.003-25.003-36.736-25.003-60.331 9.515-44.843 25.003-60.331 36.736-25.003 60.331-25.003 44.843 9.515 60.331 25.003 25.003 36.736 25.003 60.331z"></path>
               </svg>
-              <span className="home-text65 section-Text">
+              <span className="home-contact-method-text section-Text">
                 <span>Phoenix, AZ</span>
               </span>
             </div>
-            <a
-              href="mailto:robert@rochadrums.com?subject=rochadrums request"
-              className="home-cta-btn1 anchor button"
-            >
-              Contact
-            </a>
+            <ContactButton />
           </div>
         </div>
       </div>
-      <div className="home-section-separator2"></div>
+      <div className="home-section-separator"></div>
       <div className="home-footer-container">
         <div className="home-footer">
           <div className="home-social-links"></div>
           <div className="home-copyright-container">
-            <svg viewBox="0 0 1024 1024" className="home-icon6">
+            <svg viewBox="0 0 1024 1024" className="home-copyright-icon">
               <path d="M512 854q140 0 241-101t101-241-101-241-241-101-241 101-101 241 101 241 241 101zM512 86q176 0 301 125t125 301-125 301-301 125-301-125-125-301 125-301 301-125zM506 390q-80 0-80 116v12q0 116 80 116 30 0 50-17t20-43h76q0 50-44 88-42 36-102 36-80 0-122-48t-42-132v-12q0-82 40-128 48-54 124-54 66 0 104 38 42 42 42 98h-76q0-14-6-26-10-20-14-24-20-20-50-20z"></path>
             </svg>
             <span className="anchor">
