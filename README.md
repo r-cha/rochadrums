e80a2b08395dd345b90a769637cfb8018bdc825b
r-cha <robert@akchandler.com>
2023-02-28 20:25:20 -0700

    Adapt the astro homepage template for rochadrums

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# rochadrums-React
+# rochadrums Landing Page
+
+[![Built with Astro](https://astro.badg.es/v1/built-with-astro.svg)](https://astro.build)
+
+A fork I guess
+
+> An Astro + Tailwind CSS example/template for landing pages.
+
+
+## Commands
+
+| Command                | Action                                            |
+| :--------------------- | :------------------------------------------------ |
+| `npm install`          | Install dependencies                              |
+| `npm run dev`          | Start local dev server at `localhost:3000`        |
+| `npm run build`        | Build your production site to `./dist/`           |
+| `npm run preview`      | Preview your build locally, before deploying      |
+| `npm run astro ...`    | Run CLI commands like `astro add`, `astro check`  |
+| `npm run astro --help` | Get help using the Astro CLI                      |
+| `npm run format`       | Format code with [Prettier](https://prettier.io/) |
+| `npm run clean`        | Remove `node_modules` and build output            |
---
+++ tailwind.config.cjs
@@ -0,0 +1,45 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "var(--color-primary)",
+        secondary: "var(--color-secondary)",
+        bar: {
+          300: "#ff8866",
+          400: "#ff7466",
+          500: "#f66763",
+          600: "#c3514e",
+          800: "#c3514e",
+        },
+        fill: {
+          300: "#2f72e0",
+          400: "#275fba",
+          500: "#1f4c94",
+          600: "#143161",
+          800: "#09172d",
+        }
+      },
+      textColor: {
+        default: "var(--color-text)",
+        offset: "var(--color-text-offset)",
+      },
+      backgroundColor: {
+        default: "var(--color-background)",
+        offset: "var(--color-background-offset)",
+      },
+      borderColor: {
+        default: "var(--color-border)",
+      },
+    },
+  },
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [require("tailwindcss-fluid-type")],
+};
