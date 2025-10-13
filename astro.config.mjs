f67664541e90e7c59aaa9d685c5322d59df54677
Robert Chandler <robert@akchandler.com>
2025-10-12 20:05:09 -0700

    update astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,3 @@
-import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
@@ -8,9 +7,6 @@ export default defineConfig({
   site: "https://www.rochadrums.com/",
   integrations: [
     tailwind(),
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     sitemap(),
   ],
   vite: {
